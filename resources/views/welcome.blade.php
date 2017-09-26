4f4d49da0ecc7534ddd8386a2bea894acfd308f1
Adam Marshall <adam@appsynergy.net>
2017-09-26 11:19:28 +0100

    Clean up welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,20 @@
 
 @section('content')
     <div class="container">
-        <div class="my-2 card">
+        <div class="my-4 p-4 card">
             <h1>Hello, world!</h1>
-            <h2>{{ $foo }} &middot; {{ $fii }}</h2>
+
+            <p>Right now, there's two things you can do here:</p>
+
+            <ol class="list-group">
+                <li class="list-group-item">
+                    <a href="{{ url('admin') }}">Log in to the administration area.</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{ url('submit/1/edit') }}">See an example of the assessment form.</a>
+                </li>
+            </ol>
+
         </div>
     </div>
 @endsection
