2f7f660446082c8b0b07692957827dca287fece4
Adam Marshall <adam@appsynergy.net>
2017-10-26 10:33:48 +0100

    readable down msg

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -10,7 +10,7 @@
         padding: 0;
         width: 100%;
         height: 100%;
-        color: #B0BEC5;
+        color: #012837;
         display: table;
         font-weight: 100;
         font-family: 'Lato';
