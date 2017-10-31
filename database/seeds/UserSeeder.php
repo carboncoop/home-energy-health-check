73884165e3be354f305360b2d13a6299ba4ec424
Adam Marshall <adam@appsynergy.net>
2017-10-31 13:09:00 +0000

    Hashed pw seeds

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Adam',
             'email' => 'adam@appsynergy.net',
-            'password' => env('PW_INIT', 'change me'),
+            'password' => \Hash::make(env('PW_INIT', 'change me')),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
@@ -23,7 +23,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Jonathan',
             'email' => 'jonathan@carbon.coop',
-            'password' => env('PW_INIT', 'change me'),
+            'password' => \Hash::make(env('PW_INIT', 'change me')),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
