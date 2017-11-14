0f8f5cbe4c8d8678636182d1c608496aab4f4d29
Adam Marshall <adam@appsynergy.net>
2017-11-14 15:14:29 +0000

    Cover page needs no margins, hmm..

--- resources/views/pdf/partial/front-page.blade.php
+++ resources/views/pdf/partial/front-page.blade.php
@@ -1,18 +1,18 @@
 <div class="front-page break-after-me">
     <div class="top-section">
-        <div class="container my-5">
-            <img class="img-fluid w-25" alt="{{ config('app.assessment_title') }}"
+        <div class="container p-3">
+            <img class="img-fluid img-logo" alt="{{ config('app.assessment_title') }}"
                 src="{{ asset('img/plymouth-energy-community-logo.jpg') }}">
-            <h1 class="front-page-title my-5">
+            <h1 class="front-page-title my-4">
                 {{ config('app.assessment_title') }}
             </h1>
         </div>
     </div>
     <div class="middle-section">
-        <img class="img-fluid" src="http://via.placeholder.com/1200x200">
+        <img class="img-fluid" src="http://via.placeholder.com/1200x400">
     </div>
     <div class="bottom-section">
-        <div class="container my-5">
+        <div class="container p-3">
             @parsedown($section['body'])
         </div>
     </div>
